b156ef8873ec3944b564729dd7c086a49f55a2a2
Ajit <zlksnkwork@gmail.com>
2023-03-05 02:15:24 +0530

    static, differently

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 var text string
@@ -15,10 +17,16 @@ type HomeVars struct {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(404)
+		return
+	}
+
 	t, err := template.ParseFiles("index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Print("Template parsing error:", err)
+		log.Println("Template parsing error:", err)
+		return
 	}
 
 	err = t.Execute(w, HomeVars{
@@ -32,7 +40,8 @@ func set(w http.ResponseWriter, r *http.Request) {
 	}
 	b, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+		return
 	}
 	text = string(b)
 }
@@ -42,10 +51,29 @@ func get(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, text)
 }
 
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	data, err := os.ReadFile(path[1:])
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
+	if strings.HasSuffix(path, "js") {
+		w.Header().Set("Content-Type", "text/javascript")
+	} else {
+		w.Header().Set("Content-Type", "text/css")
+	}
+	_, err = w.Write(data)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/set", set)
 	http.HandleFunc("/get", get)
-	// log.Fatal(http.ListenAndServe(":1111", nil))
+	http.HandleFunc("/static/", staticHandler)
+	// log.Fatal(http.ListenAndServe(":2222", nil))
 	log.Fatal(http.ListenAndServeTLS(":1111", "../../sd.alai-owl.ts.net.crt", "../../sd.alai-owl.ts.net.key", nil))
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+## To use this
+
+1. Generate certs using tailscale for the computer you'd run this on
+1. Properly locate those certs in the program (main.go)
+1. Configure it on startup https://github.com/ajitid/dotfiles/blob/main/scripts/scripts/startup.fish
